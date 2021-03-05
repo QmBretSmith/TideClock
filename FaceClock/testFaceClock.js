19c23007655eff2974f695ccdb7839e254d96207
Bret Smith <Qm_Bret_Smith@yahoo.com>
2021-03-05 12:59:24 -0500

    Change Clock face to Tides rather than Numbers in CSS

--- FaceClock/testFaceClock.js
+++ FaceClock/testFaceClock.js
@@ -1,3 +1,18 @@
+const lat = 41.31539571168505;
+const lng = -73.37471490164708;
+//const today = Math.floor((new Date()).getTime() / 1000);
+
+
+fetch(`https://api.stormglass.io/v2/tide/extremes/point?lat=${lat}&lng=${lng}&start=2021-03-05&end=2021-03-15`, {
+  headers: {
+    'Authorization': '367bea9c-7aa0-11eb-b399-0242ac130002-367beb64-7aa0-11eb-b399-0242ac130002'
+  }
+}).then((response) => response.json()).then((jsonData) => {
+  // Do something with response data.
+  const results = jsonData.data.map(x => { return {...x, time: moment(x.time)} });
+  console.log(results);
+});
+
 setInterval(setClock, 1000)
 
 const hourHand = document.querySelector('[data-hour-hand]')
@@ -19,24 +34,3 @@ function setRotation(element, rotationRatio) {
 }
 
 setClock()
-
-
-
-
-
-
-
-const lat = 41.31539571168505;
-const lng = -73.37471490164708;
-//const today = Math.floor((new Date()).getTime() / 1000);
-
-
-fetch(`https://api.stormglass.io/v2/tide/extremes/point?lat=${lat}&lng=${lng}&start=2021-03-04&end=2021-03-14`, {
-  headers: {
-    'Authorization': '367bea9c-7aa0-11eb-b399-0242ac130002-367beb64-7aa0-11eb-b399-0242ac130002'
-  }
-}).then((response) => response.json()).then((jsonData) => {
-  // Do something with response data.
-  const results = jsonData.data.map(x => { return {...x, time: moment(x.time)} });
-  console.log(results);
-});
