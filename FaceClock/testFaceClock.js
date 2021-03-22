e5b1731a9b91ba27daa30421ef0a5f5e308d27c2
Bret Smith <Qm_Bret_Smith@yahoo.com>
2021-03-22 15:26:20 -0400

    turn relevant tide events into moments, calibrate now

--- FaceClock/testFaceClock.js
+++ FaceClock/testFaceClock.js
@@ -1,8 +1,7 @@
 const lat = 41.31539571168505;
 const lng = -73.37471490164708;
-const today = new moment().subtract(13, 'hours').utc().format("YYYY-MM-DDTHH:mm:ss.SSS[Z]");
+const today = new moment().subtract(2, 'days').utc().format("YYYY-MM-DDTHH:mm:ss.SSS[Z]");
 const endDate = new moment().add(5, 'days').utc().format("YYYY-MM-DDTHH:mm:ss.SSS[Z]");
-
 
 fetch(`https://api.stormglass.io/v2/tide/extremes/point?lat=${lat}&lng=${lng}&start=${today}&end=${endDate}`, {
   headers: {
@@ -12,21 +11,25 @@ fetch(`https://api.stormglass.io/v2/tide/extremes/point?lat=${lat}&lng=${lng}&st
   // Do something with response data.
   const results = jsonData.data.map(x => { return {...x, time: moment(x.time)} });
   const hightides = results.filter(results => results.type === "high");
-  console.table(hightides);
+  const now = moment()
+  const whereWouldNowBe = hightides.findIndex(elem => elem.time.isAfter(now));
+  console.log(whereWouldNowBe)
+  const prevHigh = moment(hightides[whereWouldNowBe - 1].time._d);
+  const nextHigh = moment(hightides[whereWouldNowBe].time._d);
+  console.log(prevHigh, nextHigh);
 
 
-setInterval(setClock, 1000)
+setInterval(setClock, 60000)
 
 const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
-const secondHand = document.querySelector('[data-second-hand]')
+
 
 function setClock() {
-  const currentDate = new Date()
-  const secondsRatio = currentDate.getSeconds() / 60
-  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
-  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
-  setRotation(secondHand, secondsRatio)
+  const currentTide = nextHigh.diff(now, 'minutes');
+  console.log(currentTide);
+  const minutesRatio = (currentTide.minutes()) / 60
+  const hoursRatio = (minutesRatio + currentTide.hours()) / 12
   setRotation(minuteHand, minutesRatio)
   setRotation(hourHand, hoursRatio)
 }
